f7eb07b54952a31fcbed7b5600187a5974862ab9
Informatica <informatica@alumno.local>
2025-04-08 10:43:20 -0600

    Se realizo el Login y register de manera exitosa

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ import logo from '../assets/img/perroMadriso.png'
 export const Logo = ({text}) => {
     return(
         <div className="auth-form-logo-container">
-            <img src = {logo} alt='PerroChristian'/>
+            <img src = {logo} alt='PerroChristian' style={{maxWidth: 150}}/>
             <span> {text} </span>
         </div>
     )
